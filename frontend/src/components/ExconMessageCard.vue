66cb81654486c248934c6388d788dcd5d8626f6a
Mikkel Madsen <m@madsn.net>
2017-08-23 17:49:24 +0400

    Inputfield and buttons for editing message

--- frontend/src/components/ExconMessageCard.vue
+++ frontend/src/components/ExconMessageCard.vue
@@ -2,8 +2,22 @@
   <v-flex xs12 fill-height>
     <v-card class="elevation-10">
       <div class="display-3 grey--text text--darken-1 text-xs-center">Message from EXCON
+        <v-btn fab primary dark medium @click.native="editingMessage = true" style="margin-top: -8px" v-if="!editingMessage">
+          <v-icon>edit</v-icon>
+        </v-btn>
+        <v-btn fab error dark medium @click.native="editingMessage = false" style="margin-top: -8px" v-if="editingMessage">
+          <v-icon>cancel</v-icon>
+        </v-btn>
+        <v-btn fab success dark medium @click.native="editingMessage = false" style="margin-top: -8px" v-if="editingMessage">
+          <v-icon>save</v-icon>
+        </v-btn>
       </div>
-      <h2 class="text-xs-center">{{message}}</h2>
+      <h2 class="text-xs-center" v-if="!editingMessage">{{message}}</h2>
+      <v-text-field v-if="admin && editingMessage"
+                    name="input-7-1"
+                    :value="message"
+                    multi-line
+      ></v-text-field>
     </v-card>
   </v-flex>
 </template>
@@ -20,6 +34,11 @@
       ...mapGetters({
         message: 'message'
       })
+    },
+    data () {
+      return {
+        editingMessage: false
+      }
     },
     components: {
       ExconCardTitle
