ac958947fb41df5790614f947b209628b378b442
Mikkel Madsen <m@madsn.net>
2017-08-23 18:15:36 +0400

    new message textarea styling

--- frontend/src/components/ExconMessageCard.vue
+++ frontend/src/components/ExconMessageCard.vue
@@ -2,7 +2,7 @@
   <v-flex xs12 fill-height>
     <v-card class="elevation-10">
       <div class="display-3 grey--text text--darken-1 text-xs-center">Message from EXCON
-        <v-btn fab primary dark medium @click.native="editingMessage = true" style="margin-top: -8px" v-if="!editingMessage">
+        <v-btn fab primary dark medium @click.native="beginEditingMessage" style="margin-top: -8px" v-if="!editingMessage">
           <v-icon>edit</v-icon>
         </v-btn>
         <v-btn fab error dark medium @click.native="editingMessage = false" style="margin-top: -8px" v-if="editingMessage">
@@ -14,9 +14,13 @@
       </div>
       <h2 class="text-xs-center" v-if="!editingMessage">{{message}}</h2>
       <v-text-field v-if="admin && editingMessage"
-                    name="input-7-1"
-                    :value="message"
+                    name="new-message-input"
+                    v-model="newMessage"
+                    :max="maxMessageLength"
+                    :maxlength="maxMessageLength"
+                    rows="2"
                     multi-line
+                    counter
       ></v-text-field>
     </v-card>
   </v-flex>
@@ -37,7 +41,9 @@
     },
     data () {
       return {
-        editingMessage: false
+        editingMessage: false,
+        maxMessageLength: 128,
+        newMessage: ''
       }
     },
     components: {
@@ -46,7 +52,17 @@
     methods: {
       ...mapActions({
         setMessage: 'setMessage'
-      })
+      }),
+      beginEditingMessage: function () {
+        this.newMessage = this.message
+        this.editingMessage = true
+      }
     }
   }
 </script>
+
+<style>
+  textarea[name='new-message-input'] {
+    font-size: 56px;
+  }
+</style>
