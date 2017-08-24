ec8768c139c88e37c10a8db017bf8a40d2e44403
Mikkel Madsen <m@madsn.net>
2017-08-24 06:41:43 +0400

    input field for setting event number

--- frontend/src/components/ExconCurrentEventCard.vue
+++ frontend/src/components/ExconCurrentEventCard.vue
@@ -1,7 +1,15 @@
 <template>
   <v-card class="elevation-10" v-if="admin">
-    <excon-card-title title="Current event #"></excon-card-title>
-    <v-card-text>
+    <div class="display-3 grey--text text--darken-1 text-xs-center">Current event #
+      <v-btn fab primary dark medium @click.native="beginEditingEvent" class="edit-message-button">
+        <v-icon>edit</v-icon>
+      </v-btn>
+    </div>
+    <v-card-text v-if="editingEvent">
+      <v-text-field name="new-event-input"
+      ></v-text-field>
+    </v-card-text>
+    <v-card-text v-else>
       <v-layout class="text-xs-center" style="max-height: 73px; margin-top: -24px">
         <v-flex xs-3>
           <v-btn fab dark medium primary @click="decrementEventNumber">
@@ -42,11 +50,26 @@
         eventNumber: 'eventNumber'
       })
     },
+    data: function () {
+      return {
+        editingEvent: false
+      }
+    },
     methods: {
       ...mapActions({
         decrementEventNumber: 'decrementEventNumber',
         incrementEventNumber: 'incrementEventNumber'
-      })
+      }),
+      beginEditingEvent: function () {
+        this.editingEvent = true
+      }
     }
   }
 </script>
+<style>
+  input[name='new-event-input'] {
+    font-size: 56px;
+    padding-bottom: 32px;
+    padding-top: 24px;
+  }
+</style>
